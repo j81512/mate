a396efaa56e3ac99fd4d6edf4d984aaee8e12fbb
parkdogyun <suenkill@gmail.com>
2020-10-12 21:21:24 +0900

    ERP-EmpList 작업분

--- mate/src/main/java/com/kh/mate/erp/model/dao/ErpDAO.java
+++ mate/src/main/java/com/kh/mate/erp/model/dao/ErpDAO.java
@@ -1,5 +1,7 @@
 package com.kh.mate.erp.model.dao;
 
+import java.util.List;
+
 import com.kh.mate.erp.model.vo.EMP;
 
 public interface ErpDAO {
@@ -7,5 +9,7 @@ public interface ErpDAO {
 	int insertEmp(EMP emp);
 
 	EMP selectOneEmp(String empId);
+
+	List<EMP> empList();
 
 }
--- mate/src/main/java/com/kh/mate/erp/model/dao/ErpDAOImpl.java
+++ mate/src/main/java/com/kh/mate/erp/model/dao/ErpDAOImpl.java
@@ -1,5 +1,7 @@
 package com.kh.mate.erp.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -21,6 +23,11 @@ public class ErpDAOImpl implements ErpDAO {
 	@Override
 	public int insertEmp(EMP emp) {
 		return sqlSession.insert("emp.insertEmp", emp);
+	}
+
+	@Override
+	public List<EMP> empList() {
+		return sqlSession.selectList("emp.empList");
 	}
 	
 }
